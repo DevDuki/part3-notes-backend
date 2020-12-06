8a4dfea3c990369831dacc743b5c1f661a212206
DevDuki <dusan-misic@hotmail.com>
2020-12-06 16:44:33 +0100

    finished part 3c, Database with MongoDB

--- index.js
+++ index.js
@@ -25,9 +25,7 @@ let notes = [
   }
 ]
 
-app.use(express.json())
-
-//Middlewares
+//Middlewares (orders of app.use(<Middleware>) matters, so be careful)
 
 //this middleware is used to show every propperty of the request, after parsing it in json
 const requestLogger = (request, response, next) => {
@@ -43,7 +41,20 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: "unkown endpoint" })
 }
 
+//this middleware is used to handle all errors
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if(error.name === "CastError"){
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 app.use(express.static('build')) // Make the backend use the build directory (which was copied from frontent), whenever a request is received
+
+app.use(express.json())
 
 app.use(cors()) // Cross origin resource changing: from frontend (port 3000) to backend (port3001)
 
@@ -83,20 +94,44 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if(note){ // if not note could be found by the database "note" will be null
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  response.status(204).end()
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true})
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.use(unknownEndpoint)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
