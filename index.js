665c3062329607c8cf5ea5c2302f6e4d66b7e94d
DevDuki <dusan-misic@hotmail.com>
2020-12-07 20:30:03 +0100

    part3 done, added ESLint and finished mongoDB

--- index.js
+++ index.js
@@ -4,50 +4,29 @@ const app = express()
 const cors = require('cors')
 const Note = require('./models/note')
 
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    date: "2019-05-30T17:30:31.098Z",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only Javascript",
-    date: "2019-05-30T18:39:34.091Z",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    date: "2019-05-30T19:20:14.298Z",
-    important: true
-  }
-]
-
 //Middlewares (orders of app.use(<Middleware>) matters, so be careful)
 
 //this middleware is used to show every propperty of the request, after parsing it in json
 const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method);
-  console.log('Path:', request.path);
-  console.log('Body:', request.body);
-  console.log('---');
+  console.log('Method:', request.method)
+  console.log('Path:', request.path)
+  console.log('Body:', request.body)
+  console.log('---')
   next()
 }
 
 //this middleware is used to handle all requests with not yet handled endpoints
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: "unkown endpoint" })
+  response.status(404).send({ error: 'unkown endpoint' })
 }
 
 //this middleware is used to handle all errors
 const errorHandler = (error, request, response, next) => {
   console.error(error.message)
 
-  if(error.name === "CastError"){
+  if(error.name === 'CastError'){
     return response.status(400).send({ error: 'malformatted id' })
-  } else if(error.name === "ValidationError"){
+  } else if(error.name === 'ValidationError'){
     return response.status(400).json({ error: error.message })
   }
 
@@ -63,10 +42,10 @@ app.use(cors()) // Cross origin resource changing: from frontend (port 3000) to 
 app.use(requestLogger)
 
 // const generateId = () => {
-//   const maxId = notes.length > 0 
+//   const maxId = notes.length > 0
 //     ? Math.max(...notes.map(n => n.id))
 //     : 0
-  
+
 //   return maxId + 1
 // }
 
@@ -107,7 +86,7 @@ app.get('/api/notes/:id', (request, response, next) => {
 
 app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndRemove(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
@@ -121,7 +100,7 @@ app.put('/api/notes/:id', (request, response, next) => {
     important: body.important
   }
 
-  Note.findByIdAndUpdate(request.params.id, note, { new: true})
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
     .then(updatedNote => {
       response.json(updatedNote.toJSON())
     })
--- mongo.js
+++ mongo.js
@@ -7,10 +7,10 @@ if(process.argv.length < 3){
 
 const password = process.argv[2]
 
-const url = 
+const url =
   `mongodb+srv://fullstack:${password}@fullstackopencluster.x9dwg.mongodb.net/note-app?retryWrites=true&w=majority`
 
-mongoose.connect(url, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true})
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
 const noteSchema = new mongoose.Schema({
   content: String,
