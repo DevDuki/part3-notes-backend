b3a2b7e0c86e2759218270fe308cc823151e8f22
DevDuki <dusan-misic@hotmail.com>
2020-12-18 23:36:50 +0100

    added users and refactored the app with usage of those users

--- tests/note_api.test.js
+++ tests/note_api.test.js
@@ -2,9 +2,11 @@ const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 const api = supertest(app)
+const bcrypt = require('bcrypt')
 const helper = require('./test_helper')
 
 const Note = require('../models/note')
+const User = require('../models/user')
 
 beforeEach(async () => {
   await Note.deleteMany({})
@@ -130,6 +132,61 @@ describe('deletion of a note', () => {
     expect(contents).not.toContain(noteToDelete.content)
   })
 })
+
+describe('wehn there is initially one used in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
+  })
+
+  test('creation sccueeds with a fresh username', async () => {
+    const usersatStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'duki',
+      name: 'DevDuki',
+      password: '1234'
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersatStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+
+  test('creation fails with proper statuscode and message if username is already taken', async () => {
+    const usersatStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'superuser',
+      password: '1234'
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersatStart.length)
+  })
+})
+
 
 afterAll(() => {
   mongoose.connection.close()
